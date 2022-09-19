a3565b130947704cda221f8afc4d7725ac4ede52
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-19 09:43:07 -0700

    tell  the size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+int x;
+char b;
+long int c;
+long long int y;
+float z;
+
+printf("size of char is=%i byte\n", sizeof(b));
+printf("size of int is=%i byte\n", sizeof(x));
+printf("size of long int is=%i byte\n", sizeof(c));
+printf("size of long long int is=%i byte\n", sizeof(y));
+printf("size of float is=%i byte\n", sizeof(z));
+return (0);
+}
