4bdfc6be1caf86ad352cbb99e1c8a39024345d9b
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-19 08:15:19 -0700

    making it betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - this is void
+ * description - this is a script that will return zero
+ * Return: Always 0 (Success)
+ *this is a puts function that returns 0 and prints a given message
+*/
 int main(void)
 {
-  puts("Programming is like building a muiltilingual puzzle");
-  return (0);
+puts("\"Programming is like building a muiltilingual puzzle");
+return (0);
 }
