20d8277caa39bc2f49e562b7639b960f3611fcb3
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-19 09:49:47 -0700

    spell check?

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int y;
 float z;
 
-printf("size of a char: %i byte(s)\n", sizeof(b));
-printf("size of an int: %i byte(s)\n", sizeof(x));
-printf("size of a long int: %i byte(s)\n", sizeof(c));
-printf("size of a long long int: %i byte(s)\n", sizeof(y));
-printf("size of a float: %i byte(s)\n", sizeof(z));
+printf("Size of a char: %i byte(s)\n", sizeof(b));
+printf("Size of an int: %i byte(s)\n", sizeof(x));
+printf("Size of a long int: %i byte(s)\n", sizeof(c));
+printf("Size of a long long int: %i byte(s)\n", sizeof(y));
+printf("Size of a float: %i byte(s)\n", sizeof(z));
 return (0);
 }
