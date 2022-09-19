9b11618eb52a5efb18990be5166e18d6c97bbb64
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-19 09:44:46 -0700

    adding(s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int y;
 float z;
 
-printf("size of char is=%i byte\n", sizeof(b));
-printf("size of int is=%i byte\n", sizeof(x));
-printf("size of long int is=%i byte\n", sizeof(c));
-printf("size of long long int is=%i byte\n", sizeof(y));
-printf("size of float is=%i byte\n", sizeof(z));
+printf("size of char is=%i byte(s)\n", sizeof(b));
+printf("size of int is=%i byte(s)\n", sizeof(x));
+printf("size of long int is=%i byte(s)\n", sizeof(c));
+printf("size of long long int is=%i byte(s)\n", sizeof(y));
+printf("size of float is=%i byte(s)\n", sizeof(z));
 return (0);
 }
