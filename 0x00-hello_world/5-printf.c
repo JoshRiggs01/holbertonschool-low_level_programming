5cde4c08604c766e3c04b52f46403729ad46f22f
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-19 08:39:42 -0700

    extra space for betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - is void as this is not needed just yet
+ * main - this is void
  * printf - should print the given statement
  * return - will return zero and statrt a new line
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
