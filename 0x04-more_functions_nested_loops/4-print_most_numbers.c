affc47766fd5075519f46538d4d4d8dff0bc7e2c
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-25 15:54:57 -0700

    01356789

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,22 @@
 #include "main.h"
+/**
+ * print_numbers - function that checks fo 1-9
+ *
+ * Return: Always (0)
+ *
+ */
+void print_most_numbers(void)
+{
+        int x = '0';
+        while (x <= '9')
+        {
+		if (x == '2' || x == '4')
+		{
+			x++;
+				continue;
+		}
+                _putchar(x);
+		x++;
+        }
+_putchar('\n');
+}
