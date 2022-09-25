1bfb3b7d88dc22f2e73c824942f73c7a20bddb49
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-25 14:43:23 -0700

    digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit
+ * main - entry point
+ *@c: digit
+ * Return: (1) for success (0) for fail
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
