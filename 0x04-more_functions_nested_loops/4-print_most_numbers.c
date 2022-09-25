be8e1fa117efba0cb6b8df91294599ac757a1a7d
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-25 15:59:54 -0700

    betty compliant

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,16 @@
  */
 void print_most_numbers(void)
 {
-        int x = '0';
-        while (x <= '9')
-        {
-		if (x == '2' || x == '4')
-		{
-			x++;
-				continue;
-		}
-                _putchar(x);
-		x++;
-        }
+int x = '0';
+while (x <= '9')
+{
+if (x == '2' || x == '4')
+{
+x++;
+continue;
+}
+_putchar(x);
+x++;
+}
 _putchar('\n');
 }
