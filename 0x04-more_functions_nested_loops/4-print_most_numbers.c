a3b329e843bfc39947ffb2db51b639f511d4fe33
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-28 13:01:02 -0700

    fixed spacing

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_most_numbers(void)
 {
- int x;
- for (x = '0'; x <= '9'; x++)
- {
- if (!(x == '2' || x == '4'))
- _putchar(x);
- }
- _putchar('\n');
- }
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+if (!(x == '2' || x == '4'))
+_putchar(x);
+}
+_putchar('\n');
+}
