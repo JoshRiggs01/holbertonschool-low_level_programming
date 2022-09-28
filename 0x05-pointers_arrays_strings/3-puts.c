5658a0da7beff4828738b4a2a2778d812d9e31bf
JoshRiggs01 <5574@holbertonstudents.com>
2022-09-27 20:19:35 -0700

    betty style coding

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _puts = function- prints string
+ * _puts - function- prints string
  * @str: declaring str for puts
  * Return: Always 0
  */
 void _puts(char *str)
+
 {
 	int x;
 	for (x = 0; str[x] != 0; x++)
