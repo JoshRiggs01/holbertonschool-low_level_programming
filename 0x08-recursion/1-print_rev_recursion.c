448de584367f4727cd6949460beaa235779dc213
JoshRiggs01 <5574@holbertonstudents.com>
2022-10-06 14:05:41 -0700

    reverse reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - function to print string through recusrion
+ * @s: string to take in and pass through if statement
+ * Return: recusion
+ */
+void _print_rev_recursion(char *s)
+{
+        if (*s == '\0')
+        {
+		return;
+        }
+	_print_rev_recursion(s + 1);
+        _putchar(*s);
+}
