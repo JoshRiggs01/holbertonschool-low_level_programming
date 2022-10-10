e7e0c6fe0f59f986fcf2e722a0561f690cfd2ef9
JoshRiggs01 <5574@holbertonstudents.com>
2022-10-10 14:24:55 -0700

    hope this works

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strcpy - Copies a string pointed to by src, including the
+*           terminating \n,pointed to by @dest.
+* @dest: A buffer to copy the string to.
+* @src: The source string to copy.
+*
+* Return: dest.
+*/
+char *_strcpy(char *dest, char *src)
+
+{
+int i;
+
+i = 0;
+        while (src[i])
+        {
+                dest[i] = src[i];
+                i++;
+        }
+dest[i] = '\0';
+return (dest);
+}
