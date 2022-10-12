889bcdd8fefbb0472266c5ac5a89395215dfa5e2
JoshRiggs01 <5574@holbertonstudents.com>
2022-10-11 19:51:01 -0700

    looks right to me

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
