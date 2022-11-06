7ad7b454ad354e04126d40b09366ac420e13efb6
JoshRiggs01 <5574@holbertonstudents.com>
2022-11-05 22:16:56 -0700

    function to count number of elements in a linked linst

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - prints number of elements in linked list
+ * @c: counter
+ * @h: pointer to string
+ *
+ * Return: counter
+ */
+size_t list_len(const list_t *h)
+{
+	int c;
+
+	c = 0;
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
