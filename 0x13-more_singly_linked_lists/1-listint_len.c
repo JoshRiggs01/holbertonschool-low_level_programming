7ca447b24decae4cfa2a519676b196e61f544557
JoshRiggs01 <5574@holbertonstudents.com>
2022-11-08 12:31:17 -0800

    length of lsit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,12 +2,13 @@
 
 /**
  * listint_len - length of a list
- * @: pointer to list
+ * @h: pointer to list
  * Return: size of list
  */
- size_t listint_len(const listint_t *h)
- {
+size_t listint_len(const listint_t *h)
+{
 	size_t len = 0;
+
 	while (h)
 	{
 		len++;
