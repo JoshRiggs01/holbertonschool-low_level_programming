92359c9a4353cf8b786b6029333eb314981654cf
JoshRiggs01 <5574@holbertonstudents.com>
2022-11-12 20:56:22 -0800

    betty fix

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
-        size_t c = 0;
+	size_t c = 0;
 
-        for (i = 0; h; i++)
+	for (i = 0; h; i++)
 	{
 		printf("%d\n", h->n);
-                h = h->next;
-                c++;
-        }
-        return (c);
+		h = h->next;
+		c++;
+	}
+	return (c);
 }
