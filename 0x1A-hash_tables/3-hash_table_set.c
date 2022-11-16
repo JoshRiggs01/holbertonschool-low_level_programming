184659a587368973f4b90914eb8a85a832f2bbca
JoshRiggs01 <5574@holbertonstudents.com>
2022-11-15 20:06:44 -0800

    fixed 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,76 +1,44 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - a value in a given index
+ * hash_table_set - add elements to hash table
  * @ht: hash table
- * @key: string to evaluate
- * @value: value to insert
- * Return: value
+ * @key: key
+ * @value: value associeted to the key to add
+ * Return: 0(Fails) 1(Succes)
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index, size;
+	hash_node_t *node, *temp;
 
-	unsigned long int index = 0;
-	hash_node_t *node = NULL, *tmp = NULL, *spot;
-
-	if (!ht || !ht->array || !ht->size || !key || !*key || !value)
+	if (!key || !value || !ht)
 		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-
-	tmp = node;
-	while (tmp)
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+	temp = ht->array[index];
+	while (temp)
 	{
-		if (strcmp(tmp->key, (char *)key) == 0)
+		if (temp && strcmp(key, temp->key) == 0)
 		{
-			free(tmp->value);
-			tmp->value = strdup((char *)value);
-			if (!tmp->value)
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
 				return (0);
 			return (1);
 		}
-		tmp = tmp->next;
+		temp = temp->next;
 	}
-	spot = allocate(key, value);
-	if (!spot)
-		return (0);
-	spot->next = node;
-	ht->array[index] = spot;
-
-	return (1);
-}
-
-/**
- * allocate - function that allocates memory
- * @key: key in the hash
- * @value: value of that key
- *
- * Return: node
- */
-
-hash_node_t *allocate(const char *key, const char *value)
-{
-	hash_node_t *node = NULL;
-
 	node = malloc(sizeof(hash_node_t));
-	if (!node)
-		return (NULL);
-
-	node->key = strdup((char *)key);
-	if (!node->key)
-		return (NULL);
-
-	node->value = strdup((char *)value);
-	if (!node->value)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-
-	node->next = NULL;
-
-	return (node);
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+		return (0);
+	node->value = strdup(value);
+	if (node->value == NULL)
+		return (0);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
