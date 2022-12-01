245cbb55287dc708461810571cda605a6b455b57
JoshRiggs01 <5574@holbertonstudents.com>
2022-12-01 11:31:12 -0800

    asking for new correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,12 +10,13 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int x, y, s = 0;
+	ssize_t x, y;
+	int s = 0;
 
 	if (!filename)
 		return (-1);
 
-        x = open(filename, O_WRONLY | O_APPEND);
+        x = open(filename, O_RDWR | O_APPEND);
 	if (x < 0)
 		return (-1);
 
@@ -24,11 +25,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[s])
 			s++;
 		y = write(x, text_content, s);
-		if (y != s)
+		if (y < 0)
 			return (-1);
 	}
-
-	close(x);
 
 	return (1);
 }
